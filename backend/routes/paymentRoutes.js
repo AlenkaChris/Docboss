ea3d3ea3c85adc9d7d08f55da453bc1c841c0b29
AlenkaChris <alenkanaen@gmail.com>
2026-06-24 06:05:25 +0100

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -2,16 +2,15 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/create-payment", async (req, res) => {
-  try {
-    res.json({
-      message: "Payment route ready"
-    });
-  } catch (error) {
-    res.status(500).json({
-      message: error.message
-    });
-  }
-});
+const {
+  createPayment
+} = require(
+  "../controllers/paymentController"
+);
+
+router.post(
+  "/create-payment",
+  createPayment
+);
 
 module.exports = router;

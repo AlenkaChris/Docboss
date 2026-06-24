0c4664918b0109611c4d3d17f5f48d758fed67e7
AlenkaChris <alenkanaen@gmail.com>
2026-06-24 06:12:16 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const paymentRoutes = require("./routes/paymentRoutes");
 const productRoutes = require("./routes/productRoutes");
+const orderRoutes = require("./routes/orderRoutes");
 const connectDB = require("./config/db");
 connectDB();
 const app = express();
@@ -11,6 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/api/payments", paymentRoutes);
+app.use("/api/orders", orderRoutes);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);

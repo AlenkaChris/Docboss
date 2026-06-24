70d6f9836af67a27d061b8c0c7855f7cc135faa9
AlenkaChris <alenkanaen@gmail.com>
2026-06-24 06:10:42 +0100

    Create orderRoutes.js

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const router = express.Router();
+
+const authMiddleware = require(
+  "../middleware/authMiddleware"
+);
+
+const {
+  getMyOrders
+} = require(
+  "../controllers/orderController"
+);
+
+router.get(
+  "/my-orders",
+  authMiddleware,
+  getMyOrders
+);
+
+module.exports = router;

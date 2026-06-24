b0c768c72bc69aef1cb42c97fe094adab11201a7
AlenkaChris <alenkanaen@gmail.com>
2026-06-24 05:43:34 +0100

    Create productController.js

---
+++ backend/controllers/productController.js
@@ -0,0 +1,46 @@
+const Product = require("../models/Product");
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({
+      active: true
+    });
+
+    res.json(products);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+exports.getProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(
+      req.params.id
+    );
+
+    res.json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};
+
+exports.createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(
+      req.body
+    );
+
+    res.status(201).json(product);
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
+  }
+};

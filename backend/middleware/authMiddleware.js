e978f1e64d0cf1f8fbb0c0643e44e27febc8ff44
AlenkaChris <alenkanaen@gmail.com>
2026-06-25 14:55:02 +0100

    Update authMiddleware.js

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,27 +1,26 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
-  const token = req.headers.authorization;
+  const token = req.header("Authorization");
 
   if (!token) {
     return res.status(401).json({
-      message: "Unauthorized"
+      message: "No token provided"
     });
   }
 
   try {
     const decoded = jwt.verify(
       token,
-      "DOCBOSS_SECRET"
+      process.env.JWT_SECRET || "docbosssecret"
     );
 
     req.user = decoded;
 
     next();
-
   } catch (error) {
     return res.status(401).json({
-      message: "Invalid Token"
+      message: "Invalid token"
     });
   }
 };
